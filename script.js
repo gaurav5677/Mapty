2f20a3e39299813f982efc940477cf3ad50bf548
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-04-29 09:46:01 +0530

    Using the Geolocation API

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// using The Geolocation API 
+if (navigator.geolocation)
+   navigator.geolocation.getCurrentPosition(function (position) {
+      // console.log(position);
+
+      // destructuring , so this will create  a varriable call latitude based 
+      // out of the latitude property of this object. 
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+
+
+
+   }
+      , function () {
+         alert('Could not get your position')
+      })
